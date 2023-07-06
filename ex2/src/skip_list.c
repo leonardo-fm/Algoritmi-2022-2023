3ca8a9ea89c6ac458ca636dd7d1486571bb99b7b
Leonardo Ferrero-Merlino <leonardo.ferrerom@edu.unito.it>
2023-07-06 11:14:37 +0200

    Finish fisrt draft

--- ex2/src/skip_list.c
+++ ex2/src/skip_list.c
@@ -4,6 +4,7 @@
 
 static size_t random_level(size_t max_height);
 static struct Node* create_node(void *item, size_t lvl);
+static void clear_node(struct Node *node);
 
 void new_skiplist(struct SkipList *list, size_t max_height, int (*compar)(const void*, const void*)) {
     if(max_height > 0) {
@@ -22,77 +23,46 @@ void new_skiplist(struct SkipList *list, size_t max_height, int (*compar)(const 
 }
 
 void clear_skiplist(struct SkipList *list) {
-    
+    for (int i = 0; i < list->max_level - 1; i++) {
+        clear_node(list->heads[i]);
+    }
+    free(list);
 }
 
 void insert_skiplist(struct SkipList *list, void *item) {
-
     struct Node *newNode = create_node(item, random_level(list->max_height));
     if (newNode->size > list->max_level) {
         list->max_level = newNode->size;
     }
 
     struct Node **currentNode = list->heads;
-    for (int k = list->max_level - 1; k >= 0; k--) {
-        if (currentNode[k] == NULL || item < currentNode[k]->item) {
-            if (k < newNode->size) {
-                newNode->next[k] = currentNode[k];
-                currentNode[k] = newNode;
-            } else {
-                currentNode = currentNode[k]->next;
-                k++;
-            }
+    for (int i = list->max_level - 1; i >= 0; i--) {
+        if (currentNode[i] == NULL || currentNode[i]->item >= item) {
+            if (newNode->size >= i) {
+                newNode->next[i] = currentNode[i];
+                currentNode[i] = newNode;
+            }          
+        } else {
+            currentNode = currentNode[i]->next;
+            i++;
         }
     } 
-
-    /*
-    insertSkipList(list, item):
-    new = createNode(item, randomLevel(list->max_height))
-    if new->size > list->max_level:
-        list->max_level = new->size
-
-    x = list->heads
-    for k = list->max_level downto 1:
-        if x[k] == NULL or item < x[k]->item:
-            if k < new->size:
-              new->next[k] = x[k]
-              x[k] = new
-        else:
-            x = x[k]->next
-            k++
-     */
 }
 
 const void* search_skiplist(struct SkipList *list, void *item) {
-    struct SkipList *currentList = list;
-    struct Node **currentNode = currentList->heads;
-    for (int i = currentList->max_level - 1; i >= 0; i--) {
+    struct Node **currentNode = list->heads;
+    for (int i = list->max_level - 1; i >= 0; i--) {
         while (currentNode[i]->next[i]->item != NULL
-            && currentList->compare(currentNode[i]->next[i]->item, item) <= 0) {
+            && list->compare(currentNode[i]->next[i]->item, item) <= 0) {
             currentNode = currentNode[i]->next;
         }
     }
     
-    if (currentNode[1]->item == item) {
-        return currentNode[1]->item;
+    if (currentNode[0]->item == item) {
+        return currentNode[0]->item;
     } else {
         return NULL;
     }
-    /*
-    searchSkipList(list, item):
-    x = list->heads
-
-    // loop invariant: x[i]->item <= item or item < first element of level i in list
-    for i = list->max_level downto 1:
-        while x[i]->next[i] != NULL and x[i]->next[i]->item <= item:
-            x = x[i]->next
-
-    // loop end: x[1]->item <= item or item < first element in list
-    if x[1]->item == item then
-        return x[1]->item
-    else
-        return failure
-    */
 }
 
 static size_t random_level(size_t max_height) {
@@ -108,4 +78,11 @@ static struct Node* create_node(void *item, size_t lvl) {
     newNode->item = item;
     newNode->size = lvl;
     return newNode;
+}
+
+static void clear_node(struct Node *node) {
+    if (node->next != NULL) {
+        clear_node(node->next);
+    }
+    free(node);
 }
